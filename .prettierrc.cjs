26f68ab918af979070e9b43ca835b77245a6b73d
hyoban <hi@hyoban.cc>
2023-02-08 17:59:09 +0800

    chore: move some prettier config to editorconfig

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,8 +1,5 @@
 /** @type {import("@ianvs/prettier-plugin-sort-imports").PrettierConfig} */
 module.exports = {
-  printWidth: 80,
-  tabWidth: 2,
-  useTabs: false,
   semi: false,
   singleQuote: true,
   quoteProps: 'as-needed',
