97e355525336f9b2daa2e58370b71f297fe5a1a1
hyoban <hi@hyoban.cc>
2023-02-18 16:35:50 +0800

    style: sidebar nav

--- src/components/SidebarNav.tsx
+++ src/components/SidebarNav.tsx
@@ -4,11 +4,9 @@ import { Menu } from "antd";
 import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 
-import { useDark } from "@/hooks";
-
 type MenuItem = Required<MenuProps>["items"][number];
 
-function getItem(
+function menu(
   label: React.ReactNode,
   key: string,
   icon?: React.ReactNode,
@@ -25,30 +23,23 @@ function getItem(
 }
 
 const items: MenuItem[] = [
-  getItem(
-    "TODO",
-    "todo",
-    <div className="i-mdi-calendar-today-outline"></div>,
-    [getItem("Item 1", "/example/todos/1"), getItem("List", "/example/todos/")]
-  ),
+  menu("TODO", "todo", <div className="i-mdi-calendar-today-outline"></div>, [
+    menu("Item 1", "/example/todos/1"),
+    menu("List", "/example/todos/"),
+  ]),
 ];
 
 const SidebarNav = () => {
-  const { isDark } = useDark();
   const location = useLocation();
   const [current, setCurrent] = useState(location.pathname);
 
-  const onClick: MenuProps["onClick"] = (e) => {
-    setCurrent(e.key);
-  };
-
   return (
     <Menu
-      theme={isDark ? "dark" : "light"}
-      onClick={onClick}
+      onClick={(e) => setCurrent(e.key)}
       selectedKeys={[current]}
       mode="inline"
       items={items}
+      defaultOpenKeys={["todo"]}
       className="h-full w-64 hidden md:block"
     />
   );
