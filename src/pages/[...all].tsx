316fffca97e4959efde9e3a5eb50e36751928d71
hyoban <hi@hyoban.cc>
2023-04-14 08:22:29 +0800

    chore: clean for v2

--- src/pages/[...all].tsx
+++
@@ -1,9 +0,0 @@
-function NotFoundPage() {
-	return (
-		<div className="flex flex-col gap-4 h-screen items-center justify-center">
-			<h1 className="text-xl">oops, page not found</h1>
-		</div>
-	)
-}
-
-export default NotFoundPage
