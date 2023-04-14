ff55cc7c6dc8ad96643d45b697c19f0e0230eb8c
hyoban <hi@hyoban.cc>
2023-04-14 10:22:44 +0800

    chore: setup eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+	extends: [
+		"eslint:recommended",
+		"plugin:react/recommended",
+		"plugin:react/jsx-runtime",
+		"plugin:jsx-a11y/recommended",
+		"plugin:react-hooks/recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:@typescript-eslint/recommended-type-checked",
+	],
+	plugins: ["react", "@typescript-eslint", "react-refresh"],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		project: true,
+		tsconfigRootDir: __dirname,
+	},
+	root: true,
+	settings: {
+		react: {
+			version: "detect",
+		},
+	},
+	rules: {
+		"react-refresh/only-export-components": "warn",
+	},
+}
