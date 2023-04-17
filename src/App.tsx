dc8afcf4e4f0a8bb73752d0d0f8b8ef500ea83f7
hyoban <hi@hyoban.cc>
2023-04-17 21:07:01 +0800

    chore: jotai and tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import { User } from "./type"
+
+import { Getter, atom, useAtom } from "jotai"
+import { atomsWithQuery } from "jotai-tanstack-query"
+import ky from "ky"
+
+const idAtom = atom(1)
+
+const [userAtom] = atomsWithQuery((get: Getter) => ({
+	queryKey: ["users", get(idAtom)],
+	queryFn: async ({ queryKey: [, id] }) => {
+		const res = await ky.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+		return res.json<User>()
+	},
+}))
+
 export default function App() {
+	const [data] = useAtom(userAtom)
 	return (
-		<div className="hover:(bg-gray-400 font-medium) font-(light mono) text-2xl">
-			<p>hello from UnoCSS</p>
-			<div className="i-carbon-sun"></div>
+		<div>
+			<h1>{data?.name}</h1>
 		</div>
 	)
 }
