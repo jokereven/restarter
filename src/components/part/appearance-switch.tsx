613cf8dae083e752c098a5ea9eb0b7e001e2ead8
Stephen Zhou <hi@hyoban.cc>
2023-06-02 20:17:14 +0800

    chore: update

--- src/components/part/appearance-switch.tsx
+++ src/components/part/appearance-switch.tsx
@@ -1,12 +1,13 @@
-import { useDark } from "@/hooks/index"
+import { useDark } from "@/hooks/useDark"
 
 export default function AppearanceSwitch() {
-	const [isDark, toggleDark] = useDark()
+	const [, toggleDark] = useDark()
 
 	return (
-		<button
-			className={isDark ? "i-carbon-moon" : "i-carbon-sun"}
-			onClick={toggleDark}
-		></button>
+		<button onClick={toggleDark} className="flex">
+			<div className="i-carbon-sun rotate-0 scale-100 transition-transform duration-500 dark:-rotate-90 dark:scale-0" />
+			<div className="i-carbon-moon absolute rotate-90 scale-0 transition-transform duration-500 dark:rotate-0 dark:scale-100" />
+			<span className="sr-only">Toggle theme</span>
+		</button>
 	)
 }
