7ff58520cc0c5c59626fe8bc93ff852d4f614a04
Stephen Zhou <hi@hyoban.cc>
2023-06-02 13:29:17 +0800

    feat: add electron support

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,30 @@ import react from "@vitejs/plugin-react-swc"
 import path from "node:path"
 import UnoCSS from "unocss/vite"
 import { defineConfig } from "vite"
+import electron from "vite-plugin-electron"
+import renderer from "vite-plugin-electron-renderer"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), UnoCSS()],
+	plugins: [
+		react(),
+		electron([
+			{
+				// Main-Process entry file of the Electron App.
+				entry: "electron/main.ts",
+			},
+			{
+				entry: "electron/preload.ts",
+				onstart(options) {
+					// Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete,
+					// instead of restarting the entire Electron App.
+					options.reload()
+				},
+			},
+		]),
+		renderer(),
+		UnoCSS(),
+	],
 	resolve: {
 		alias: {
 			"@/": `${path.resolve(__dirname, "src")}/`,
