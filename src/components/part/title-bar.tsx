91f69b8fbb42578385d1977236d51e1650a36165
Stephen Zhou <hi@hyoban.cc>
2023-06-30 21:04:05 +0800

    chore: remove title bar border

--- src/components/part/title-bar.tsx
+++ src/components/part/title-bar.tsx
@@ -1,7 +1,7 @@
 export default function TitleBar() {
 	return (
 		<h1
-			className="text-center border-border border-b py-2 text-sm"
+			className="text-center py-2 text-sm"
 			style={{
 				// @ts-expect-error -webkit-app-region is not in the CSS spec
 				WebkitAppRegion: "drag",
