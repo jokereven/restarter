6138e97c1c3d45ee4cfaa1e283b8c3d3ac80db20
Stephen Zhou <hi@hyoban.cc>
2023-07-01 09:36:01 +0800

    chore: add tailwind icons

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import "./i18n"
 import "./styles/globals.css"
 
 import App from "@/App"
-import TitleBar from "@/components/part/title-bar"
 import React from "react"
 import ReactDOM from "react-dom/client"
 
@@ -10,7 +9,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
 		<>
 			{/* remove this if you are not using Electron */}
-			<TitleBar />
+			{/* <TitleBar /> */}
 			<App />
 		</>
 	</React.StrictMode>
